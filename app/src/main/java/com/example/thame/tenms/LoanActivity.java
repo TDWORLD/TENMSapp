779484999bca66b50fc0aac32a843d6e9443a142
spoofywoofy <social.methmal@gmail.com>
2017-09-12 23:29:28 +0530

    lll

--- app/src/main/java/com/example/thame/tenms/LoanActivity.java
+++ app/src/main/java/com/example/thame/tenms/LoanActivity.java
@@ -2,7 +2,9 @@ package com.example.thame.tenms;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,16 +19,24 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class LoanActivity extends AppCompatActivity {
 
-    String dataList[] = new String[]{"LO001 - LKR 150,000","LO002 - LKR 250,000","LO003 - LKR 50,000","LO004 - LKR 10,000"
-            ,"LO005 - LKR 1,000","LO006 - LKR 500,000","LO007 - LKR 1,100,000","LO008 - LKR 60,000","LO009 - LKR 8,500"
-            ,"LO010 - LKR 750,000"};
+    // Declaring connection variables
+    Connection con;
+    DataAccess db;
+
+    String dataList[] = new String[]{"","","","","","","","","",""};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loan);
+
+        db = new DataAccess();
 
         ActionBar actionbar = getSupportActionBar();
         actionbar.setLogo(R.drawable.ico_loan32);
@@ -37,6 +47,8 @@ public class LoanActivity extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.listitem,R.id.Item,dataList);
         final Spinner spinner = (Spinner) findViewById(R.id.loanItem);
         spinner.setAdapter(adapter);
+
+        setLoan();
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -50,6 +62,69 @@ public class LoanActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void setLoan(){
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.listitem,R.id.Item,dataList);
+        final Spinner spinner2 = (Spinner) findViewById(R.id.loanItem);
+
+        try {
+            // Change below query according to your own database.
+
+            String query = "SELECT LoanID,ProposedAmount FROM Loan";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            int arrayValue = 0;
+
+            if (rs!=null) {
+                try{
+                    while(rs.next()){
+                        String Lid = rs.getString("LoanID");
+                        String LAmt = rs.getString("ProposedAmount");
+                        dataList[arrayValue] = Lid+" - "+LAmt;
+                        arrayValue += 1;
+                    }
+                }catch (Exception ex){
+                    AlertDialog alertDialog2 = new AlertDialog.Builder(LoanActivity.this).create();
+                    alertDialog2.setTitle("Error");
+                    alertDialog2.setMessage("Error: "+ex.toString());
+                    alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog2.show();
+
+                }
+
+            } else {
+                AlertDialog alertDialog2 = new AlertDialog.Builder(LoanActivity.this).create();
+                alertDialog2.setTitle("Invalid");
+                alertDialog2.setMessage("Invalid query");
+                alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog2.show();
+            }
+        } catch (Exception ex) {
+            AlertDialog alertDialog2 = new AlertDialog.Builder(LoanActivity.this).create();
+            alertDialog2.setTitle("Connection error");
+            alertDialog2.setMessage(ex.toString());
+            alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog2.show();
+
+        }
+
+        spinner2.setAdapter(adapter);
     }
 }
 
