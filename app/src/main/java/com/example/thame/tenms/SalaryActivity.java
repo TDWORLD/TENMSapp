7f8096b848591bd5d9046d239481f2edce19d008
spoofywoofy <social.methmal@gmail.com>
2017-09-30 21:42:14 +0530

    Salary - Partially edited

--- app/src/main/java/com/example/thame/tenms/SalaryActivity.java
+++ app/src/main/java/com/example/thame/tenms/SalaryActivity.java
@@ -1,7 +1,9 @@
 package com.example.thame.tenms;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,17 +19,21 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SalaryActivity extends AppCompatActivity {
 
     String chartHeader[] = {"Basic","Allowance","No Pay","OT","Performance","Loan","ETF / EPF"};
-    int charValue[] = {65,5,3,10,5,10,2};
+    int charValue[] = {0,0,0,0,0,0,0,0,0,0,0,0};
     String TotalSalary = "25000";
     int chartColor[] = {Color.rgb(44, 197, 78),Color.rgb(219, 84, 68),Color.rgb(13, 140, 231),Color.rgb(242, 103, 38),Color.rgb(80, 85, 87),Color.rgb(0, 161, 168),Color.rgb(100, 36, 201)};
 
-    String YearList[] = new String[]{"2014","2015","2016","2017"};
+    //String YearList[] = new String[]{"2014","2015","2016","2017"};
+    ArrayList<String> YearList = new ArrayList<String>();
     String MonthList[] = new String[]{"January","February","March","April","May","June","July","August","September","October","November","December"};
 
     String basicsal =  "Basic Salary Amount        : "+"30000";
@@ -39,6 +45,10 @@ public class SalaryActivity extends AppCompatActivity {
     String etf =       "ETF / EPF Amount             : "+"1000";
     String totalsal =  "Total Salary Amount   : "+TotalSalary;
 
+    // Declaring connection variables
+    Connection con;
+    DataAccess db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +59,9 @@ public class SalaryActivity extends AppCompatActivity {
         actionbar.setDisplayUseLogoEnabled(true);
         actionbar.setDisplayShowHomeEnabled(true);
         actionbar.setTitle("  Salary");
+
+        db = new DataAccess();
+        con =  db.getConnection();
 
         setUpYear();
         setUpMonth();
@@ -99,14 +112,86 @@ public class SalaryActivity extends AppCompatActivity {
     }
 
     private void setUpYear(){
+
+        if (con == null) {
+            AlertDialog alertDialog2 = new AlertDialog.Builder(SalaryActivity.this).create();
+            alertDialog2.setTitle("Connection error");
+            alertDialog2.setMessage("Check your internet access");
+            alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog2.show();
+        }else{
+            try {
+                // Change below query according to your own database.
+                String query = "SELECT DISTINCT TOP 10 Year FROM Performance ORDER BY Year DESC";
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                int arrayValue = 0;
+
+                if (rs!=null) {
+
+                    try{
+                        while(rs.next()){
+                            String year = rs.getString("Year");
+                            YearList.add(arrayValue,year);
+                            arrayValue += 1;
+                        }
+                    }catch (Exception ex){
+                        AlertDialog alertDialog2 = new AlertDialog.Builder(SalaryActivity.this).create();
+                        alertDialog2.setTitle("Error");
+                        alertDialog2.setMessage("No Performance Data: "+ex.toString());
+                        alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog2.show();
+
+                    }
+
+                } else {
+                    AlertDialog alertDialog2 = new AlertDialog.Builder(SalaryActivity.this).create();
+                    alertDialog2.setTitle("Invalid");
+                    alertDialog2.setMessage("Invalid query");
+                    alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog2.show();
+                }
+            } catch (Exception ex) {
+                AlertDialog alertDialog2 = new AlertDialog.Builder(SalaryActivity.this).create();
+                alertDialog2.setTitle("Connection error");
+                alertDialog2.setMessage("Please check your internet connection");
+                alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog2.show();
+
+            }
+        }
+
         ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.listitem,R.id.Item,YearList);
-        final Spinner spinner = (Spinner) findViewById(R.id.salaryYear);
+        final Spinner spinner = (Spinner) findViewById(R.id.performYear);
         spinner.setAdapter(adapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String val = spinner.getSelectedItem().toString();
 
+                getData(val);
+                setupChart();
             }
 
             @Override
@@ -114,6 +199,80 @@ public class SalaryActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void getData(String Year){
+
+        if (con == null) {
+            AlertDialog alertDialog2 = new AlertDialog.Builder(SalaryActivity.this).create();
+            alertDialog2.setTitle("Connection error");
+            alertDialog2.setMessage("Check your internet access");
+            alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog2.show();
+        }else{
+            try {
+
+                // Change below query according to your own database.
+                String query = "SELECT Month, Performance FROM Performance WHERE EmpID = ' AND Year = '"+Year+"' ORDER BY Month ASC";
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                for(int i=0; i<12;i++){
+                    charValue[i] = 0;
+                }
+                if (rs!=null) {
+
+                    try{
+                        while(rs.next()){
+                            String performance = rs.getString("Performance");
+                            int month = Integer.parseInt(rs.getString("Month"));
+                            double dd = Double.parseDouble(performance);
+                            charValue[month] = (int) dd;
+                        }
+                    }catch (Exception ex){
+                        AlertDialog alertDialog2 = new AlertDialog.Builder(SalaryActivity.this).create();
+                        alertDialog2.setTitle("Error");
+                        alertDialog2.setMessage("No Performance Data: "+ex.toString());
+                        alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog2.show();
+
+                    }
+
+                } else {
+                    AlertDialog alertDialog2 = new AlertDialog.Builder(SalaryActivity.this).create();
+                    alertDialog2.setTitle("Invalid");
+                    alertDialog2.setMessage("Invalid query");
+                    alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog2.show();
+                }
+            } catch (Exception ex) {
+                AlertDialog alertDialog2 = new AlertDialog.Builder(SalaryActivity.this).create();
+                alertDialog2.setTitle("Connection error");
+                alertDialog2.setMessage("Please check your internet connection");
+                alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog2.show();
+
+            }
+        }
     }
 
     private void setUpMonth(){
